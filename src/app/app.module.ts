881bd70e22b1bb7f6633f96fc916ba7429e6a830
Nicolai <aikeradvend@gmail.com>
2021-11-23 01:55:48 +0200

    Front Template Created

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { ButtonComponent } from './components/button/button.component';
+import { GroupItemComponent } from './components/group-item/group-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    GroupsComponent,
+    ButtonComponent,
+    GroupItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
