0ab69d3723ad45580c43bc376fe73c7b5a6a2071
Nicolai <aikeradvend@gmail.com>
2021-11-25 21:23:27 +0200

    enforced http

--- src/app/components/groups/groups.component.ts
+++ src/app/components/groups/groups.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { MatTable } from "@angular/material/table";
-import { group } from "console";
 
 export class Group {
   constructor(
@@ -13,36 +12,12 @@ export class Group {
     public GroupYear: number
   ) {}
 }
-// const GROUP_DATA: Group[] = [
-
-// ]
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
-];
-
 @Component({
   selector: "app-groups",
   templateUrl: "./groups.component.html",
   styleUrls: ["./groups.component.css"],
 })
 export class GroupsComponent implements OnInit {
-  displayedColumns: string[] = ["position", "name", "weight", "symbol"];
-  dataSource = [...ELEMENT_DATA];
 
   groups?: Group[] = [];
   constructor(private HttpClient: HttpClient) {}
@@ -59,15 +34,5 @@ export class GroupsComponent implements OnInit {
       }
     );
   }
-  @ViewChild(MatTable) table!: MatTable<PeriodicElement>;
-  addData() {
-    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
-    this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
-    this.table.renderRows();
-  }
 
-  removeData() {
-    this.dataSource.pop();
-    this.table.renderRows();
-  }
 }
