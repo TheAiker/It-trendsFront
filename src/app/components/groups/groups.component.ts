7ec73166323df72e6db1fec71480dbd92cc6de83
Nicolai <aikeradvend@gmail.com>
2021-11-27 00:54:55 +0200

    get students

--- src/app/components/groups/groups.component.ts
+++ src/app/components/groups/groups.component.ts
@@ -1,6 +1,13 @@
-import { HttpClient } from "@angular/common/http";
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { MatTable } from "@angular/material/table";
+import { HttpClient, HttpEvent } from "@angular/common/http";
+import { Component, OnInit, Injectable } from "@angular/core";
+import {
+  MatTreeFlatDataSource,
+  MatTreeFlattener,
+} from "@angular/material/tree";
+import { SelectionModel } from "@angular/cdk/collections";
+import { FlatTreeControl } from "@angular/cdk/tree";
+import { BehaviorSubject } from "rxjs";
+import { Student } from "../students/students.component";
 
 export class Group {
   constructor(
@@ -9,7 +16,8 @@ export class Group {
     public Year: number,
     public Program: string,
     public President: string,
-    public GroupYear: number
+    public GroupYear: number,
+    public Students: Student[]
   ) {}
 }
 @Component({
@@ -18,21 +26,26 @@ export class Group {
   styleUrls: ["./groups.component.css"],
 })
 export class GroupsComponent implements OnInit {
-
   groups?: Group[] = [];
+  selectedStudents: Student[] = [];
   constructor(private HttpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.getGroups();
   }
 
-  getGroups() {
-    this.HttpClient.get<Array<Group>>("http://localhost:4200/api/Home/Groups").subscribe(
-      response => {
-        console.log(response);
-        this.groups = response;
-      }
-    );
+  onGroupClick(group: Group): void {
+    this.selectedStudents = group.Students;
   }
 
+  getGroups() {
+    this.HttpClient.get<Array<Group>>(
+      "http://localhost:4200/api/Home/Groups"
+    ).subscribe((response) => {
+      console.log(response);
+      this.groups = response;
+    });
+  }
 }
+let toggler = document.getElementsByClassName("caret");
+let i: number;
--- src/app/components/students/students.component.ts
+++ src/app/components/students/students.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+export class Student {
+  constructor(
+    public Id: number,
+    public FirstName: string,
+    public LastName: string,
+    public PhoneNumber: number,
+    public GroupForeignKey: number,
+  ){}
+}
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
@@ -7,6 +16,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentsComponent implements OnInit {
 
+  students?: Student[] = [];
   constructor() { }
 
   ngOnInit(): void {
